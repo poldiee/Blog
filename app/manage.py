7cd30c57d9830e6651a276de51fefe7454384e16
poldiee <nessidave@gmail.com>
2022-05-18 07:56:32 +0300

    creation of signup and login form

--- app/manage.py
+++ app/manage.py
@@ -1,11 +1,15 @@
 from flask_script import Manager,Server
 
-from app import create_app
+from app import create_app,db
 
 app = create_app()
 
 manager =  Manager(app)
 manager.add_command('run',Server(use_debugger=True))
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db)
 
 if __name__ == '__main__':
     manager.run()
